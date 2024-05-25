63e8e51cc2afd9894ea1d4c01a09fc93d262d70e
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2024-05-25 19:14:13 +0200

    Added icon page for personal use

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import { HomePageP } from './pages/HomePage';
 import { ProjectPageP } from './pages/ProjectPage';
 import { ArticlePageP } from './pages/ArticlePage';
+import { IconPageP } from './pages/Debug/IconPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Link to="/"><h1>Dungeon Master</h1></Link>
       </nav>
       <Routes>
+        <Route path="icons/" element={<IconPageP />} />
         <Route index element={<HomePageP />} />
         <Route path="project/:projectId" element={<ProjectPageP />} />
         <Route path="article/:articleId" element={<ArticlePageP />} />
---
+++ src/pages/Debug/IconPage.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import "./IconPage.scss";
+import { IconC } from "../../components/shared/Icon";
+
+export function IconPageP(): JSX.Element {
+  const [icons, setIcons] = useState<string[]>([]);
+  const [copied, setCopied] = useState<number | null>(null);
+
+  useEffect(() => {
+    fetch('/symbol-defs.svg')
+      .then(response => response.text())
+      .then(data => {
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(data, 'image/svg+xml');
+        const symbols = svgDoc.getElementsByTagName('symbol');
+        const iconNames = Array.from(symbols).map(symbol => symbol.id.slice(5));
+        setIcons(iconNames);
+      })
+      .catch(error => console.error('Error loading SVG symbols:', error));
+  }, []);
+
+  function handleClick(name: string) {
+    navigator.clipboard.writeText(name);
+    setCopied(Date.now());
+    setTimeout(() => {
+      setCopied(null);
+    }, 1000);
+  }
+
+  return (
+    <div className="page">
+      <h1>Icons</h1>
+      <div className="icons">
+        {icons.map(icon => (
+          <div className="icons--icon" onClick={() => handleClick(icon)}>
+            <IconC key={icon} name={icon} size="large" />
+            <p>{icon}</p>
+          </div>
+        ))}
+      </div>
+      {copied && (
+        <p className="copied">
+          <IconC name="copy" />
+          <span>Copied to clipboard!</span>
+        </p>
+      )}
+    </div>
+  );
+}
